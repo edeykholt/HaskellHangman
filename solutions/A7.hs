542ef8551db069110d05cbda8ffb34ddac3a6d2e
Ed Eykholt <ed.eykholt@gmail.com>
2023-01-23 00:57:17 +0000

    Up to A7 Q#02

--- solutions/A7.hs
+++ solutions/A7.hs
@@ -9,16 +9,21 @@ import System.Directory ( doesFileExist )
 -- *** A7: Functors & Applicatives *** --
 
 -- Q#01
-
-getUpperChar = undefined
+getUpperChar :: IO Char
+getUpperChar = toUpper <$> getChar
+-- *A7> getUpperChar
+-- a'A'
 
 -- Q#02
-
-_DICT_ = undefined
+_DICT_ :: IO Dictionary
+_DICT_ = lines <$> readFile _DICT_FILE_
+-- *A7> _DICT_
+-- ["aardvark","aardwolf",-- ...
 
 -- Q#03
-
+makeGameIfValid :: Either GameException Secret -> Either GameException Game
 makeGameIfValid = undefined
+-- makeGameIfValid eitherGeOrS = makeGame <$> eitherGeOrS
 
 -- Q#04
 
