3344931254cccf98ab9201602ac77944a8fb1fdc
Ed Eykholt <ed.eykholt@gmail.com>
2023-01-30 00:32:12 +0000

    up to A7 Q#03 and attempt at Q#04

--- solutions/A7.hs
+++ solutions/A7.hs
@@ -4,7 +4,9 @@ import Provided
 import A6
 
 import Data.Char ( toUpper )
-import System.Directory ( doesFileExist )
+import System.Directory ( doesFileExist, exeExtension )
+import GHC.IO.Device (IODeviceType(Directory))
+import GHC.RTS.Flags (getGCFlags)
 
 -- *** A7: Functors & Applicatives *** --
 
@@ -22,9 +24,50 @@ _DICT_ = lines <$> readFile _DICT_FILE_
 
 -- Q#03
 makeGameIfValid :: Either GameException Secret -> Either GameException Game
-makeGameIfValid = undefined
--- makeGameIfValid eitherGeOrS = makeGame <$> eitherGeOrS
+makeGameIfValid eges = case eges of
+    Left ex -> Left ex
+    Right s -> case validateNoDict s of
+            Left ex2 -> Left ex2
+            Right _ -> makeGame <$> eges
+-- *A7> makeGameIfValid $ Right "asdf"
+-- Right 
+-- **************************************************
+-- 
+--        Current Guess:  _ _ _ _
+--
+--        Guessed:
+--
+--        Remaining Chances:      7
+--
+-- **************************************************
+
+-- *A7> makeGameIfValid $ Right "1"
+-- Left Invalid word. A secret word must be in the dictionary and be between 3 and 20 alphabetic characters.
+-- *A7> makeGameIfValid $ Left InvalidWord
+-- Left Invalid word. A secret word must be in the dictionary and be between 3 and 20 alphabetic characters.
+
 
 -- Q#04
+getDict :: IO (Maybe Dictionary)
+getDict = gg
+    where
+        gg :: IO (Maybe Dictionary)
+        gg = toMaybe <$> x <*> z
 
-getDict = undefined
+        x :: IO Bool
+        x = doesFileExist _DICT_FILE_
+
+        z :: IO Dictionary
+        z = lines <$> readFile _DICT_FILE_
+        -- TODO if _DICT_FILE is not found, return Just _DICT_
+
+getDict2 :: IO (Maybe Dictionary)
+getDict2 = toMaybe <$> doesFileExist _DICT_FILE_ <*> (lines <$> readFile _DICT_FILE_)
+        -- TODO if _DICT_FILE is not found, return Just _DICT_
+
+-- getDict3 :: Maybe (IO Dictionary)
+-- getDict3 = ggg
+--    where
+--        xs :: IO (Maybe Dictionary)
+--        xs = toMaybe <$> doesFileExist _DICT_FILE_ <*>  (lines <$> readFile _DICT_FILE_)
+--        ggg = undefined
